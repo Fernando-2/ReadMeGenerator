ccfe97ff03736ddebb646eeba2d6309c0a4ada56
Fernando Rivas <fernandorivascastillo011@gmail.com>
2020-09-24 18:15:16 -0400

    kinda skeleton

---
+++ index.js
@@ -0,0 +1,40 @@
+var fs = require("fs")
+var inquirer = require("inquire")
+const questions = [{
+    type:"input",
+    message:"",
+    name:"",
+},
+{
+    type:"input",
+    message:"",
+    name:"",
+},
+{
+    type:"input",
+    message:"",
+    name:"",
+},
+{
+    type:"input",
+    message:"",
+    name:"",
+},
+{
+    type:"input",
+    message:"",
+    name:"",
+}
+
+];
+
+
+inquirer
+ .prompt(questions)
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
