1927e96a4403fd5f7d8e0878a47da74723447f3e
Fernando Rivas <fernandorivascastillo011@gmail.com>
2020-09-24 18:37:59 -0400

    most questions

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ var fs = require("fs")
 var inquirer = require("inquire")
 const questions = [{
     type:"input",
-    message:"",
-    name:"",
+    message:"what is your Github username?",
+    name:"username",
+},
+{
+    type:"input",
+    message:"What is your email address?",
+    name:"emailAddress",
+},
+{
+    type:"input",
+    message:"what is your projects name?",
+    name:"projectName",
+},
+{
+    type:"input",
+    message:"Please write a short description about your project?",
+    name:"Description",
+},
+{
+    type:"input",
+    message:"What does the user need to know about using your repo",
+    name:"repoUse",
+},
+{
+    type:"input",
+    message:"What does the user need to know about contributing to the repo",
+    name:"contribution",
 },
 {
     type:"input",
@@ -20,11 +45,6 @@ const questions = [{
     message:"",
     name:"",
 },
-{
-    type:"input",
-    message:"",
-    name:"",
-}
 
 ];
 
